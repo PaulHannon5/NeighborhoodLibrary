aa4bd5cf4f1045f5270e480cc1e6db4b3a4ea516
Paul Hannon <phannon527@gmail.com>
2025-10-29 11:09:24 -0700

    Created an Array list for the books

--- src/main/java/com/library/Book.java
+++ src/main/java/com/library/Book.java
@@ -1,4 +1,20 @@
 package com.library;
 
 public class Book {
+    int id;
+    String isbn;
+    String title;
+    boolean isCheckedOut;
+    String checkedOutTo;
+
+    void checkOut(String name) {
+        isCheckedOut = true;
+        checkedOutTo = name;
+    }
+
+    void checkIn() {
+        isCheckedOut = false;
+        checkedOutTo = "";
+    }
+
 }
--- src/main/java/com/library/Main.java
+++ src/main/java/com/library/Main.java
@@ -1,9 +1,23 @@
 package com.library;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList<String> book = new ArrayList();
+
+        book.add("Secrets of the Sith");
+        book.add("Harry Potter");
+        book.add("Captain Underpants");
+        book.add("Dune");
+        book.add("Divergent");
+        book.add("How to kill a mockingbird");
+        book.add("Hunger Games");
+        book.add("Archie's Weird Mysteries");
+        book.add("Holes");
+        book.add("Amulet");
+
+        System.out.println(book);
     }
 }
