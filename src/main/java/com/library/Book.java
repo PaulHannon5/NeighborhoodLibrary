23c60b0e0497c4dd617bbefea77539b30995f91f
Paul Hannon <phannon527@gmail.com>
2025-10-29 11:46:55 -0700

    Book class completed with getters, setters, and constructors.

--- src/main/java/com/library/Book.java
+++ src/main/java/com/library/Book.java
@@ -7,14 +7,63 @@ public class Book {
     boolean isCheckedOut;
     String checkedOutTo;
 
-    void checkOut(String name) {
+    //Constructors
+    public Book(int id, String isbn, String title, boolean isCheckedOut, String checkedOutTo) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = isCheckedOut;
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    //Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    //Methods
+    public void checkOut(String name) {
         isCheckedOut = true;
         checkedOutTo = name;
     }
-
-    void checkIn() {
+    public void checkIn() {
         isCheckedOut = false;
         checkedOutTo = "";
     }
-
 }
--- src/main/java/com/library/Main.java
+++ src/main/java/com/library/Main.java
@@ -1,11 +1,21 @@
 package com.library;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        //created array list
         ArrayList<String> book = new ArrayList();
+
+        System.out.println("What book would you like to check out today?: ");
+        String bookname = scanner.next();
+        scanner.nextLine();
 
         book.add("Secrets of the Sith");
         book.add("Harry Potter");
@@ -18,6 +28,12 @@ public class Main {
         book.add("Holes");
         book.add("Amulet");
 
-        System.out.println(book);
+        scanner.close();
+
+        //book.remove(1); - removes book from  indicated index line
+        //book.set(1, "Ex Machina"); - replaces book with new book on indicated index line
+        //System.out.println(book); - prints books
+        // System.out.println(book.size()); - prints the quantity of books
+       // Collections.sort(book); - sort books in alphabetical order
+        }
     }
-}
